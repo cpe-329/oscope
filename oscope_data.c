d70cc4b9b55da1186fa35eb742652e7c9c4b7f55
Spencer Shaw <sfshaw@calpoly.edu>
2019-05-09 09:01:20 -0700

    Add data interface files

---
+++ oscope_data.c
@@ -0,0 +1,10 @@
+/*
+ * oscope_data.c
+ *
+ *  Created on: May 9, 2019
+ *      Author: sfshaw
+ */
+
+
+
+
---
+++ oscope_data.h
@@ -0,0 +1,15 @@
+/*
+ * oscope_data.h
+ *
+ *  Created on: May 9, 2019
+ *      Author: sfshaw
+ */
+
+#ifndef OSCOPE_DATA_H_
+#define OSCOPE_DATA_H_
+
+
+
+
+
+#endif /* OSCOPE_DATA_H_ */
