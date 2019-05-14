183df9da856412793949b65937974ae7d8d1eca0
dfujiwar <46456196+dfujiwar@users.noreply.github.com>
2019-05-13 23:12:32 -0700

    Update oscope_term.c

--- oscope_term.c
+++ oscope_term.c
@@ -31,6 +31,7 @@ void move_right(unsigned int val){
     printf("%c[%dC",ESC, val);
 }
 
+
 void move_home(){
     printf("%c[H",ESC);
 }
@@ -39,3 +40,69 @@ void clear_screen(){
     printf("%c[%2J",ESC);
 }
 
+void move_cursor(unsigned int x, unsigned int y){
+    move_home();
+    move_right(x);
+    move_down(y);
+}
+
+void draw_horizontal(unsigned int length, char x){
+    int i;
+    for (i=0; i < length; i++){
+        printf("%c", x);
+    }
+}
+
+void draw_vertical(unsigned int length, char x){
+    int i;
+    for (i=0; i < length; i++){
+        printf("%c", x);
+        move_down();
+
+    }
+}
+void print_border(){
+    move_home();
+    draw_horizontal(LENGTH, 'X');
+    draw_vertical(WIDTH, 'X');
+    move_home();
+    draw_vertical(WIDTH, 'X');
+    draw_horizontal(LENGTH, 'X');
+    move_cursor(40, 0);
+    draw_vertical(WIDTH-2, 'X')
+}
+
+void print_info(){
+    int y = INFO_Y_CORD;
+    move_cursor(INFO_X_CORD,y);
+    if (oscope_mode = OSCOPE_MODE_AC){
+        printf("AC MODE");
+        y+=2;
+        move_cursor(INFO_X_CORD, y);
+        printf("AC PKPK: %d"; ac_pkpk);
+        y+=2;
+        move_cursor(INFO_X_CORD, y);
+        printf("AC FREQ: %d"; ac_freq);
+        y+=2;
+        move_cursor(INFO_X_CORD, y);
+        printf("AC PERIOD: %d"; ac_period);
+
+    }
+    else{
+        printf("DC MODE");
+    }
+    y+=2;
+    move_cursor(INFO_X_CORD, y);
+    printf("DC OFFSET: %d"; dc_offset);
+    y+=2;
+    move_cursor(INFO_X_CORD, y);
+    printf("NUM SAMPLES: %d"; num_samples);
+
+
+}
+
+void init_terminal(){
+    clear_screen();
+    print_border();
+    print_info();
+}
