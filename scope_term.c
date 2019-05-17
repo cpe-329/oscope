2627dc383af38735d3e106657b7a9b07976921c2
dfujiwar <46456196+dfujiwar@users.noreply.github.com>
2019-05-17 13:14:08 -0700

    Update scope_term.c

--- scope_term.c
+++ scope_term.c
@@ -126,6 +126,7 @@ void print_info() {
         move_cursor(INFO_X_CORD, y);
         uart_write_string("AC PERIOD: ", 11);
         uart_write_int(scope_get_ac_period());
+        uart_write_sting("ms",2);
         y += 2;
         move_cursor(INFO_X_CORD, y);
         uart_write_string("DC OFFSET: ", 11);
